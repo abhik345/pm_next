72762930b625dc5ed9adf77ba88c5e5d840c8c2a
abhik345 <abhikdas345@gmail.com>
2024-11-13 16:39:50 +0530

    changes moved to start

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { parse } = require('url');
 const next = require('next');
 
 const port = parseInt(process.env.PORT || '3000', 10);
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== 'production' 
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
@@ -12,10 +12,11 @@ app.prepare().then(() => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
   });
-  server.listen(port, '0.0.0.0', (err) => {
+
+  server.listen(port, (err) => {
     if (err) throw err;
     console.log(
-      `> Server listening at http://0.0.0.0:${port} as ${
+      `> Server listening at http://localhost:${port} as ${
         dev ? 'development' : process.env.NODE_ENV
       }`
     );
